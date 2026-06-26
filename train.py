8c31077f1a8bdd7d2b5e5c4a8f14ea75e54fb700
syedfaiq <syedfaiq12345@gmail.com>
2026-06-26 10:22:33 +0800

    Complete MLflow integration with PostgreSQL and MinIO

--- train.py
+++ train.py
@@ -1,22 +1,75 @@
+import os
+import joblib
+import mlflow
+import mlflow.sklearn
+
 from sklearn.datasets import fetch_california_housing
 from sklearn.linear_model import LinearRegression
-import joblib
+from sklearn.metrics import mean_squared_error
+
 import os
 
-# Load dataset
+# MinIO Credentials
+os.environ["AWS_ACCESS_KEY_ID"] = "minioadmin"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "minio123"
+os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://127.0.0.1:9000"
+
+# ===========================
+# MLflow Configuration
+
+# ===========================
+
+mlflow.set_tracking_uri("http://localhost:5000")
+mlflow.set_experiment("House Price Prediction")
+
+
+# ===========================
+# Load Dataset
+# ===========================
+
 housing = fetch_california_housing()
 
 X = housing.data
 y = housing.target
 
-# Train model
-model = LinearRegression()
-model.fit(X, y)
 
-# Create model folder
-os.makedirs("model", exist_ok=True)
+# ===========================
+# Start MLflow Run
+# ===========================
 
-# Save model
-joblib.dump(model, "model/model.pkl")
+with mlflow.start_run():
 
-print("Model trained and saved!")
+    # Create Model
+    model = LinearRegression()
+
+    # Train Model
+    model.fit(X, y)
+
+    # Predict
+    predictions = model.predict(X)
+
+    # Calculate Metric
+    mse = mean_squared_error(y, predictions)
+
+    # Log Parameters
+    mlflow.log_param("model_type", "LinearRegression")
+
+    # Log Metrics
+    mlflow.log_metric("mse", mse)
+
+    # Save Model Locally
+    os.makedirs("model", exist_ok=True)
+    joblib.dump(model, "model/model.pkl")
+
+    # Log Model to MLflow
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        name="house-price-model"
+    )
+
+print("======================================")
+print("Training completed successfully!")
+print(f"Mean Squared Error: {mse}")
+print("Model saved to ./model/model.pkl")
+print("Logged to MLflow successfully!")
+print("======================================")
